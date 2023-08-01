50cee4a53cd370f09f4f1f177087ae06fc17280a
Linda Mei <85714837+ilindaa@users.noreply.github.com>
2023-08-01 19:47:22 -0400

    Added CSS and image loaders

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import './style.css';
+import Icon from './Images/yurucamp.jpg';
+
 console.log("Testing");
+
+const content = document.getElementById('content');
+
+const header = document.createElement('h1');
+header.textContent = 'Laid-back Camp (Yuru Camp)';
+content.appendChild(header);
+
+const myIcon = new Image();
+myIcon.src = Icon;
+content.appendChild(myIcon);
+
