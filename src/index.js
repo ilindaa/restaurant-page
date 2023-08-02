82d41409916367ecb2e8afc7e52c6b3dfcfedae7
Linda Mei <85714837+ilindaa@users.noreply.github.com>
2023-08-02 17:15:46 -0400

    Working on JS code; Added new files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 import './style.css';
-import Icon from './Images/yurucamp.jpg';
+import { initialPageLoad } from './home';
 
-console.log("Testing");
-
-const content = document.getElementById('content');
-
-const header = document.createElement('h1');
-header.textContent = 'Laid-back Camp (Yuru Camp)';
-content.appendChild(header);
-
-const myIcon = new Image();
-myIcon.src = Icon;
-content.appendChild(myIcon);
+initialPageLoad();
 
